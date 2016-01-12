e5055707a97efca087408a872f8409ac616fbd9c
Keenon Werling <keenon@stanford.edu>
2016-01-12 15:42:17 -0800

    Adding ModelLog, a new backing store for an incrementally stored set of models.

---
+++ src/main/java/com/github/keenon/loglinear/storage/ModelLog.java
@@ -0,0 +1,132 @@
+package com.github.keenon.loglinear.storage;
+
+import com.github.keenon.loglinear.model.GraphicalModel;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by keenon on 1/12/16.
+ *
+ * This is simpler than a ModelBatch, since it doesn't support deletion or editing post hoc. Files created by ModelLog
+ * can be read by ModelBatch, and vice versa.
+ *
+ * The point of ModelLog is to open a streaming queue of data, backed by a file, which can be added to with minimal
+ * disk IO. That optimizes for the common case, where we're recording a number of training examples to be used later
+ * for training or analysis, but the examples are arriving in an online fashion.
+ */
+public class ModelLog extends ArrayList<GraphicalModel> {
+    public boolean writeWithFactors = false;
+
+    OutputStream os;
+
+    /**
+     * Creates a ModelLog that writes to 'filename'. It starts with any models already there.
+     *
+     * WARNING: behavior is undefined if multiple ModelLog's are backed by the same file. Do not do this.
+     *
+     * @param filename the file to back ModelLog, will be created if it doesn't exist yet
+     * @throws IOException
+     */
+    public ModelLog(String filename) throws IOException {
+        InputStream is = new FileInputStream(filename);
+        GraphicalModel read;
+        while ((read = GraphicalModel.readFromStream(is)) != null) {
+            add(read, false);
+        }
+        is.close();
+        os = new FileOutputStream(filename, true); // second arg is "append", prevents restarting file
+    }
+
+    /**
+     * This is a constructor that's basically only for internal use (testing).
+     * @param is the input stream to read the existing models from
+     * @param os the output stream to write the models to
+     */
+    ModelLog(InputStream is, OutputStream os) throws IOException {
+        GraphicalModel read;
+        while ((read = GraphicalModel.readFromStream(is)) != null) {
+            add(read, false);
+        }
+        this.os = os;
+    }
+
+    /**
+     * Closes the file OutputStream to the backing store, which flushes any progress that's currently being cached.
+     *
+     * @throws IOException
+     */
+    public void close() throws IOException {
+        os.close();
+    }
+
+    /**
+     * Adds an element to the list, and if recordOnDisk is true, appends it to the backing store.
+     *
+     * @param m the model to add
+     * @param recordOnDisk whether or not to add to the backing disk store
+     * @return success
+     */
+    private boolean add(GraphicalModel m, boolean recordOnDisk) {
+        boolean success = super.add(m);
+        if (!success) return false;
+        if (recordOnDisk) {
+
+            // Attempt to record this to the backing log file, without the factors
+
+            try {
+                if (writeWithFactors) {
+                    m.writeToStream(os);
+                } else {
+                    Set<GraphicalModel.Factor> cachedFactors = m.factors;
+                    m.factors = new HashSet<>();
+                    m.writeToStream(os);
+                    m.factors = cachedFactors;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean add(GraphicalModel m) {
+        return add(m, true);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new IllegalStateException("Operation unsupported by ModelLog. Try ModelBatch instead.");
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new IllegalStateException("Operation unsupported by ModelLog. Try ModelBatch instead.");
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new IllegalStateException("Operation unsupported by ModelLog. Try ModelBatch instead.");
+    }
+
+    @Override
+    public void clear() {
+        throw new IllegalStateException("Operation unsupported by ModelLog. Try ModelBatch instead.");
+    }
+
+    @Override
+    public GraphicalModel set(int index, GraphicalModel element) {
+        throw new IllegalStateException("Operation unsupported by ModelLog. Try ModelBatch instead.");
+    }
+
+    @Override
+    public void add(int index, GraphicalModel element) {
+        throw new IllegalStateException("Operation unsupported by ModelLog. Try ModelBatch instead.");
+    }
+
+    @Override
+    public GraphicalModel remove(int index) {
+        throw new IllegalStateException("Operation unsupported by ModelLog. Try ModelBatch instead.");
+    }
+}
---
+++ src/test/java/com/github/keenon/loglinear/storage/ModelLogTest.java
@@ -0,0 +1,96 @@
+package com.github.keenon.loglinear.storage;
+
+import com.github.keenon.loglinear.model.GraphicalModel;
+import com.github.keenon.loglinear.model.GraphicalModelTest;
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.From;
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Generator;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by keenon on 10/17/15.
+ *
+ * This just double checks that we can write and read these model batches without loss.
+ */
+@RunWith(Theories.class)
+public class ModelLogTest {
+    @Theory
+    public void testProtoBatch(@ForAll(sampleSize = 50) @From(ModelBatchTest.BatchGenerator.class) ModelBatch batch) throws IOException {
+        ByteArrayInputStream emptyInputStream = new ByteArrayInputStream(new byte[0]);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        ModelLog log = new ModelLog(emptyInputStream, byteArrayOutputStream);
+        log.writeWithFactors = true;
+        for (GraphicalModel model : batch) {
+            log.add(model);
+        }
+        log.close();
+
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+
+        ModelLog recovered = new ModelLog(byteArrayInputStream, byteArrayOutputStream);
+        byteArrayInputStream.close();
+
+        assertEquals(batch.size(), recovered.size());
+
+        for (int i = 0; i < batch.size(); i++) {
+            assertTrue(batch.get(i).valueEquals(recovered.get(i), 1.0e-5));
+        }
+    }
+
+    @Theory
+    public void testProtoBatchCloseContinue(@ForAll(sampleSize = 50) @From(ModelBatchTest.BatchGenerator.class) ModelBatch batch) throws IOException {
+        ByteArrayInputStream emptyInputStream = new ByteArrayInputStream(new byte[0]);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        ModelLog log = new ModelLog(emptyInputStream, byteArrayOutputStream);
+        log.writeWithFactors = true;
+        for (GraphicalModel model : batch) {
+            log.add(model);
+        }
+        log.close();
+
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+
+        ByteArrayOutputStream secondByteArrayOutputStream = new ByteArrayOutputStream();
+        secondByteArrayOutputStream.write(bytes);
+        ModelLog secondLog = new ModelLog(byteArrayInputStream, secondByteArrayOutputStream);
+        secondByteArrayOutputStream.close();
+
+        secondLog.writeWithFactors = true;
+        for (GraphicalModel model : batch) {
+            secondLog.add(model);
+        }
+        secondLog.close();
+        assertEquals(batch.size() * 2, secondLog.size());
+
+        byte[] finalBytes = secondByteArrayOutputStream.toByteArray();
+        ByteArrayInputStream secondByteArrayInputStream = new ByteArrayInputStream(finalBytes);
+
+        ModelLog recovered = new ModelLog(secondByteArrayInputStream, byteArrayOutputStream);
+        secondByteArrayInputStream.close();
+
+        assertEquals(batch.size()*2, recovered.size());
+
+        for (int i = 0; i < batch.size(); i++) {
+            assertTrue(batch.get(i).valueEquals(recovered.get(i), 1.0e-5));
+        }
+        for (int i = batch.size(); i < batch.size()*2; i++) {
+            assertTrue(batch.get(i-batch.size()).valueEquals(recovered.get(i), 1.0e-5));
+        }
+    }
+
+}
